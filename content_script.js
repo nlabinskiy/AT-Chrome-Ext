f6fb3070c9f98ced8a7bd0c70372d365b9e3e020
Nikolay Labinskiy <nlabinskiy@enrise.com>
2013-07-24 12:02:18 +0300

    initial commit

---
+++ content_script.js
@@ -0,0 +1,5 @@
+document.getElementById('oauth-consumerkey').value = '1aKnep8y';
+document.getElementById('oauth-consumersecret').value = '8d429295ed7b9875d893a21bf1549284';
+document.getElementById('oauth-requestTokenUrl').value = 'http://dev.api.autotrack.nl/oauth/request';
+document.getElementById('oauth-accessTokenUrl').value = 'http://dev.api.autotrack.nl/oauth/access';
+document.getElementById('oauth-emulateSession').checked = true;
